b2d0696569a74c684025f8d939610440dc6973ab
Dream-weaver <eye_tanvir@hotmail.com>
2020-02-08 00:15:00 +0600

    StudentLoanRepayment Calculation

--- Paycompute.Services/Implementation/EmployeeService.cs
+++ Paycompute.Services/Implementation/EmployeeService.cs
@@ -8,56 +8,77 @@ using Paycompute.Persistence;
 
 namespace Paycompute.Services.Implementation
 {
-    public class EmployeeService : IEmployeeService
-    {
-        private readonly ApplicationDbContext _context;
+      public class EmployeeService : IEmployeeService
+      {
+            private readonly ApplicationDbContext _context;
+            private decimal studentLoanAmount;
 
-        public EmployeeService(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+            public EmployeeService(ApplicationDbContext context)
+            {
+                  _context = context;
+            }
 
-        public async Task CreateAsync(Employee newEmployee)
-        {
-            await _context.Employees.AddAsync(newEmployee);
-            await _context.SaveChangesAsync();
-        }
+            public async Task CreateAsync(Employee newEmployee)
+            {
+                  await _context.Employees.AddAsync(newEmployee);
+                  await _context.SaveChangesAsync();
+            }
 
-        public IEnumerable<Employee> GetAll() 
-            => _context.Employees.ToList();
-    
-        public Employee GetById(int employeeId) 
-            => _context.Employees.Where(e => e.Id == employeeId).FirstOrDefault();
+            public IEnumerable<Employee> GetAll()
+                => _context.Employees.ToList();
 
-        public async Task Delete(int employeeId)
-        {
-            var employee = GetById(employeeId);
-             _context.Remove(employee);
-            await _context.SaveChangesAsync();
-        }
+            public Employee GetById(int employeeId)
+                => _context.Employees.Where(e => e.Id == employeeId).FirstOrDefault();
 
-        public async Task UpdateAsync(Employee employee)
-        {
-            _context.Update(employee);
-            await _context.SaveChangesAsync();
-        }
+            public async Task Delete(int employeeId)
+            {
+                  var employee = GetById(employeeId);
+                  _context.Remove(employee);
+                  await _context.SaveChangesAsync();
+            }
 
-        public async Task UpdateAsync(int id)
-        {
-            var employee = GetById(id);
-            _context.Update(employee);
-            await _context.SaveChangesAsync();
-        }
+            public async Task UpdateAsync(Employee employee)
+            {
+                  _context.Update(employee);
+                  await _context.SaveChangesAsync();
+            }
 
-        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
-        {
-            throw new NotImplementedException();
-        }
+            public async Task UpdateAsync(int id)
+            {
+                  var employee = GetById(id);
+                  _context.Update(employee);
+                  await _context.SaveChangesAsync();
+            }
 
-        public decimal UnionFees(int id)
-        {
-            throw new NotImplementedException();
-        }
+            public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+            {
+                  var employee = GetById(id);
+                  if (employee.StudentLoan == StudentLoan.Yes  &&  totalAmount> 1750  &&  totalAmount < 2000)
+                  {
+                        studentLoanAmount = 15m;
+                  }
+                  else if (employee.StudentLoan == StudentLoan.Yes  &&  totalAmount >= 2000  &&  totalAmount < 2250)
+                  {
+                        studentLoanAmount = 38m;
+                  }
+                  else if (employee.StudentLoan == StudentLoan.Yes  &&  totalAmount >= 2250  &&  totalAmount < 2500)
+                  {
+                        studentLoanAmount = 60m;
+                  }
+                  else if (employee.StudentLoan == StudentLoan.Yes  &&  totalAmount >= 2500  )
+                  {
+                        studentLoanAmount = 83m;
+                  }
+                  else {
+                        studentLoanAmount = 0m; 
+                  }
+                  return studentLoanAmount; 
+            }
 
-    }
+            public decimal UnionFees(int id)
+            {
+                  throw new NotImplementedException();
+            }
+
+      }
 }
